0a52bb778f93ac58dd88d56e771efe6ee64bdbb9
jdl <jdl@JDL-DESKTOP>
2018-10-15 18:02:01 +0800

    第一次创建项目并上传

---
+++ boke/src/main/java/com/boke/service/LogService.java
@@ -0,0 +1,13 @@
+/*
+ * copyright 2018
+ * 
+ */
+package com.boke.service;
+
+/**
+ * @author jdl
+ * time: 2018年10月15日 下午5:06:49
+ */
+public class LogService {
+
+}
---
+++ boke/src/test/java/com/boke/service/LogServiceTest.java
@@ -0,0 +1,13 @@
+/*
+ * copyright 2018
+ * 
+ */
+package com.boke.service;
+
+/**
+ * @author jdl
+ * time: 2018年10月15日 下午5:08:56
+ */
+public class LogServiceTest {
+
+}
